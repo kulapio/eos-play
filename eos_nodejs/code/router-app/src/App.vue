c0dda7dee761276a8dd34b3f6650c2edf12cb9f1
Nattapon Nimakul <totizy@gmail.com>
2018-06-13 18:56:00 +0700

    Add about

--- eos_nodejs/code/router-app/src/App.vue
+++ eos_nodejs/code/router-app/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <router-link :to="{ name: 'HelloWorld' }">Home</router-link>
+    <router-link to="/about">About</router-link>
     <router-view/>
+    <div>
+      <iframe class="terminal" src="http://localhost:7681/">
+      </iframe>
+    </div>
   </div>
 </template>
 
@@ -19,5 +25,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.terminal {
+  width: 100%;
+  height: 100%
 }
 </style>
--- eos_nodejs/code/router-app/src/router/index.js
+++ eos_nodejs/code/router-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
