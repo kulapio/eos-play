24a599cf3ae79a5023c250cb61f3b0b4465d48f3
Nattapon Nimakul <totizy@gmail.com>
2018-06-14 01:16:15 +0700

    Add wallet create, import, deploy bios

--- eos_nodejs/code/router-app/src/components/HelloWorld.vue
+++ eos_nodejs/code/router-app/src/components/HelloWorld.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button @click="clicking()">Wallet list</button>
+    <div>
+      <h2>1. Wallet Create</h2>
+      <button @click="walletCreate()">Create</button>
+    </div>
+    <!-- <div>
+      <h2>Wallet Unlock</h2>
+      <input type="text" class="form-control wtext" v-model="walletUnlockPrivateKey" placeholder="private key">
+      <button @click="walletUnlock()">Unlock</button>
+    </div> -->
+    <div>
+      <h2>2. Deploy Bios</h2>
+      <button @click="deployBios()">Deploy</button>
+    </div>
+    <div>
+      <h2>Wallet Import</h2>
+      <input type="text" class="form-control wtext" v-model="walletImportPrivateKey" placeholder="private key">
+      <button @click="walletImport()">Import</button>
+    </div>
+    <div>
+      <h2>Wallet List</h2>
+      <button @click="walletList()">List</button>
+    </div>
+
   </div>
 </template>
 
@@ -11,12 +33,52 @@
     name: 'HelloWorld',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg:                  'Welcome to EOS Playground'
       }
     },
     methods: {
-      clicking() {
+      walletCreate() {
+        axios.get('http://localhost:8081/wallet/create/')
+        .then((resp) => {
+          // this.coin = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      },
+      walletUnlock() {
+        axios.get('http://localhost:8081/wallet/unlock/' + this.walletUnlockPrivateKey)
+        .then((resp) => {
+          // this.coin = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      },
+      walletImport() {
+        axios.get('http://localhost:8081/wallet/import/' + this.walletImportPrivateKey)
+        .then((resp) => {
+          // this.coin = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      },
+      walletList() {
         axios.get('http://localhost:8081/wallet/list/')
+        .then((resp) => {
+          // this.coin = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      },
+      deployBios() {
+        axios.get('http://localhost:8081/deploy/bios/')
         .then((resp) => {
           // this.coin = resp.data[0]
           console.log(resp)
--- eos_nodejs/code/server.js
+++ eos_nodejs/code/server.js
@@ -1,24 +1,54 @@
+// Load libs
 var express = require('express')
 var cors = require('cors')
 const shell = require('shelljs')
 
+// Enable access control allow origin for ajax
 var app = express();
 app.use(cors())
 
-
-app.get('/wallet/list', function (req, res) {
-  // let cmd = 'ls'
-  let eos_cmd = 'cleos wallet list'
-  let cmd = 'screen -S kulap -X stuff "' + eos_cmd + ' ^M"'
-  shell.exec( cmd )
-  res.send('Hello World2');
+// API: Wallet Create
+app.get('/wallet/create', function (req, res) {
+  let cmd = 'cleos wallet create'
+  sendCommand(cmd, req, res)
 })
 
+// API: Wallet Unlock
+app.get('/wallet/unlock/:privateKey', function (req, res) {
+  let cmd = 'cleos wallet unlock --password ' + req.params.privateKey
+  sendCommand(cmd, req, res)
+})
 
+// API: Wallet Import
+app.get('/wallet/import/:privateKey', function (req, res) {
+  let cmd = 'cleos wallet import ' + req.params.privateKey
+  sendCommand(cmd, req, res)
+})
+
+// API: Wallet List
+app.get('/wallet/list', function (req, res) {
+  let cmd = 'cleos wallet list'
+  sendCommand(cmd, req, res)
+})
+
+// API: Deploy Bios
+app.get('/deploy/bios', function (req, res) {
+  let cmd = 'cleos set contract eosio /contracts/eosio.bios -p eosio'
+  sendCommand(cmd, req, res)
+})
+
+// Send command utility
+function sendCommand(eos_cmd, req, res) {
+  let cmd = 'screen -S kulap -X stuff "' + eos_cmd + ' ^M"'
+  shell.exec( cmd )
+  res.send('ok2');
+}
+
+// Start Api server
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("API listening at http://%s:%s", host, port)
 
 })
